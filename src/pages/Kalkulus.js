bd76edf52f342d5df8f1ecfcbacdfc7e1c015320
nurul90042 <wahdanurul.nw@gmail.com>
2020-06-09 13:47:42 +0800

    changes

--- src/pages/Kalkulus.js
+++ src/pages/Kalkulus.js
@@ -3,8 +3,38 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Header from '../components/headers/Header';
 import Container from '@material-ui/core/Container';
 import Footer from '../components/Footer';
+import CalcuPict from "../components/pict/calcu.jpg";
+import Post from "../components/Post";
+import Grid from "@material-ui/core/Grid";
 
 export default function Kalkulus() {
+  let data = [
+    {
+      title: "Do You Know Calculus?",
+      post:
+        "my post data Cum sociis natoque penatibus et magnis dis parturient mo",
+    },
+    {
+      title: "what is X and Y ?",
+      post:
+        "my post data Cum sociis natoque penatibus et magnis dis parturient mo",
+    },
+    {
+      title: "my third post",
+      post:
+        "my post data Cum sociis natoque penatibus et magnis dis parturient mo",
+    },
+    {
+      title: "my four post",
+      post:
+        "my post data Cum sociis natoque penatibus et magnis dis parturient mo",
+    },
+    {
+      title: "my five post",
+      post:
+        "my post data Cum sociis natoque penatibus et magnis dis parturient mo",
+    },
+  ];
     return (
         <React.Fragment>
         <CssBaseline />
@@ -12,9 +42,10 @@ export default function Kalkulus() {
           <Header />
           
           <main>
-          <div>
-          <h1>INI PAGE Kalkulus</h1>
-        </div>
+            <Grid container spacing={2}>
+              <Post image={CalcuPict} post={data} />
+            
+            </Grid>
           </main>
       </Container>
       <Footer title="Hello" description="Something here to give the footer a purpose!" />
