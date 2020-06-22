ef83c10a4a1a6a22fd4de53839b814f7ace1bf44
nurul90042 <wahdanurul.nw@gmail.com>
2020-06-22 14:16:57 +0800

    changes

--- src/pages/Kalkulus.js
+++ src/pages/Kalkulus.js
@@ -1,54 +1,69 @@
-import React from 'react'
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Header from '../components/headers/Header';
-import Container from '@material-ui/core/Container';
-import Footer from '../components/Footer';
-import CalcuPict from "../components/pict/calcu.jpg";
-import Post from "../components/Post";
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Header from "../components/headers/Header";
+import Container from "@material-ui/core/Container";
+import Footer from "../components/Footer";
+import CalcuPict from "../components/pict/aljabar.jpg";
 import Grid from "@material-ui/core/Grid";
+import PostMateri from "../components/PostMateri";
 
-export default function Kalkulus() {
-  let data = [
+export default function Calcu() {
+  const data = [
     {
-      title: "Do You Know Calculus?",
-      post:
-        "my post data Cum sociis natoque penatibus et magnis dis parturient mo",
+      title: "Hai, Calculus",
+
+      description:
+        'Aljabar (dari bahasa arab " al-jabr " yang berarti " pengumpulan bagian yang rusak") adalah salah satu bagian dari bidang matematika yang luas, bersama-sama dengan teori bilangan,',
+      image: CalcuPict,
     },
     {
-      title: "what is X and Y ?",
-      post:
-        "my post data Cum sociis natoque penatibus et magnis dis parturient mo",
+      title: "Aljabar adalah",
+
+      description:
+        'Aljabar (dari bahasa arab " al-jabr " yang berarti " pengumpulan bagian yang rusak") adalah salah satu bagian dari bidang matematika yang luas, bersama-sama dengan teori bilangan,',
+      image: CalcuPict,
     },
     {
-      title: "my third post",
-      post:
-        "my post data Cum sociis natoque penatibus et magnis dis parturient mo",
+      title: "Aljabar adalah",
+
+      description:
+        'Aljabar (dari bahasa arab " al-jabr " yang berarti " pengumpulan bagian yang rusak") adalah salah satu bagian dari bidang matematika yang luas, bersama-sama dengan teori bilangan,',
+      image: CalcuPict,
     },
     {
-      title: "my four post",
-      post:
-        "my post data Cum sociis natoque penatibus et magnis dis parturient mo",
+      title: "Aljabar adalah",
+
+      description:
+        'Aljabar (dari bahasa arab " al-jabr " yang berarti " pengumpulan bagian yang rusak") adalah salah satu bagian dari bidang matematika yang luas, bersama-sama dengan teori bilangan,',
+      image: CalcuPict,
     },
     {
-      title: "my five post",
-      post:
-        "my post data Cum sociis natoque penatibus et magnis dis parturient mo",
+      title: "Aljabar adalah",
+
+      description:
+        'Aljabar (dari bahasa arab " al-jabr " yang berarti " pengumpulan bagian yang rusak") adalah salah satu bagian dari bidang matematika yang luas, bersama-sama dengan teori bilangan,',
+      image: CalcuPict,
     },
   ];
-    return (
-        <React.Fragment>
-        <CssBaseline />
-        <Container maxWidth="lg">
-          <Header />
-          
-          <main>
-            <Grid container spacing={2}>
-              <Post image={CalcuPict} post={data} />
-            
-            </Grid>
-          </main>
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header />
+
+        <main>
+          <Grid container spacing={4}>
+            {data.map((post) => (
+              <PostMateri key={post.title} post={post} />
+            ))}
+          </Grid>
+        </main>
       </Container>
-      <Footer title="Hello" description="Something here to give the footer a purpose!" />
+      <Footer
+        title="Hello"
+        description="Something here to give the footer a purpose!"
+      />
     </React.Fragment>
-    )
+  );
 }
