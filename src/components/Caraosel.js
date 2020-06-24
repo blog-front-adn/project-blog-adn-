7f6d2f3b25452a7089b8b5ea8abc36623a9c25bc
Amirullah23 <amirullah.kasang@gmail.com>
2020-06-24 16:25:17 +0700

    add style

--- src/components/Caraosel.js
+++ src/components/Caraosel.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Pict1 from "./pict/3.png";
-
+import Pict1 from "./pict/carosel/1.png";
+import Pict2 from "./pict/carosel/2.png";
+import Pict3 from "./pict/carosel/3.png";
+import Pict4 from "./pict/carosel/4.png";
 import "../App.css";
 
 function Caraousel() {
@@ -16,20 +18,20 @@ function Caraousel() {
       cgdcgh: "hfgdh",
     },
     {
-      pict: Pict1,
+      pict: Pict2,
       h3: "ini second slide",
       p:
         "ini first slideini first slideini first slideini first slideini first slideini first slideini first slideini first slideini first slide",
       p2: "ini baris kedua",
     },
     {
-      pict: Pict1,
+      pict: Pict3,
       h3: "ini third slide",
       p:
         "ini first slideini first slideini first slideini first slideini first slideini first slideini first slideini first slideini first slide",
     },
     {
-      pict: Pict1,
+      pict: Pict4,
       h3: "ini four slide",
       p:
         "ini first slideini first slideini first slideini first slideini first slideini first slideini first slideini first slideini first slide",
@@ -37,7 +39,7 @@ function Caraousel() {
   ];
 
   return (
-    <div>
+    <div id="div-carosel">
       <Carousel id="slideHome">
         {slide.map((data) => (
           <Carousel.Item className="class-gambar">
